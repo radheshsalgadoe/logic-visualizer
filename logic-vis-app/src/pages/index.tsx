defdbcf9243d2d9adc6770cb77e45b0c4ecc6c6a
radhesh-rmit <s3937564@student.rmit.edu.au>
2025-06-17 18:52:35 +1000

    Very Basic Template

---
+++ logic-vis-app/src/pages/index.tsx
@@ -0,0 +1,20 @@
+
+export default function Home() {
+  return (
+    <main className="
+    homeDivWrapper
+    flex
+    min-h-screen
+    items-center
+    ">
+      <h1 className="
+      flex-1 
+      text-center
+      text-8xl
+      font-courier-prime
+      ">
+        <span className="bg-yellow-300">L</span>ogic <span className="bg-yellow-300">V</span>isualizer
+      </h1>
+    </main>
+  );
+}
