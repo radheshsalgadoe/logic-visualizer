c878e84c65bc13456878b5561dfd3a4058ddd0fc
radhesh-rmit <s3937564@student.rmit.edu.au>
2025-06-17 19:16:14 +1000

    Added version number

--- logic-vis-app/src/components/text/Title.tsx
+++ logic-vis-app/src/components/text/Title.tsx
@@ -6,14 +6,21 @@ interface TitleProps {
 
 const Title = ( { size }: TitleProps) => {
   return (
-    <h1 className={`
+    <>
+    <div className={`
+      flex
+      flex-col
       flex-1 
       text-center
       text-${size}
       font-courier-prime
       `}>
-        <span className="bg-yellow-300">L</span>ogic <span className="bg-yellow-300">V</span>isualizer<span className="animate-pulse">_</span>
+    <h1 className='flex-4'>
+        <span className="bg-yellow-300">L</span>ogic <span className="bg-yellow-300">V</span>isualizer<span className="animate-ping">_</span>
       </h1>
+    <h2 className='flex-1 text-4xl font-courier-prime'>v_0.1</h2>
+    </div>
+    </>
   )
 }
 
