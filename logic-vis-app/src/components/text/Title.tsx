11e6aa0412e3e9dc92a8abfd81426b5107c6c7c3
radhesh-rmit <s3937564@student.rmit.edu.au>
2025-06-30 18:24:56 +1000

    Added Background Particles

--- logic-vis-app/src/components/text/Title.tsx
+++ logic-vis-app/src/components/text/Title.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
 interface TitleProps {
     size: string;
 }
 
 const Title = ( { size }: TitleProps) => {
+
   return (
     <>
-    <div className={`
+    <div className="
       flex
       flex-col
       flex-1 
       text-center
-      text-${size}
       font-courier-prime
-      `}>
-    <h1 className='flex-4'>
+      ">
+    <h1 className={`flex-4 text-8xl`}>
         <span className="bg-yellow-300">L</span>ogic <span className="bg-yellow-300">V</span>isualizer<span className="animate-ping">_</span>
       </h1>
     <h2 className='flex-1 text-4xl font-courier-prime'>v_0.1</h2>
--- logic-vis-app/src/pages/index.tsx
+++ logic-vis-app/src/pages/index.tsx
@@ -1,14 +1,38 @@
 import Title from "@/components/text/Title";
+import Particles from "@/components/Particles";
 
 export default function Home() {
   return (
-    <main className="
-    homeDivWrapper
-    flex
+  <div
+  className="
+    relative
+    w-full
     min-h-screen
+    flex
     items-center
-    ">
-      <Title size={"8xl"}/>
-    </main>
+    justify-center
+  "
+>
+  {/* Background Particles (ABSOLUTE & FULL SIZE) */}
+  <div className="absolute inset-0 z-0">
+    <Particles
+      particleColors={['#fbff00', '#fbff00']}
+      particleCount={200}
+      particleSpread={10}
+      speed={0.1}
+      particleBaseSize={100}
+      moveParticlesOnHover={false}
+      alphaParticles={true}
+      disableRotation={true}
+    />
+  </div>
+
+  {/* Overlay Title */}
+  <div className="z-10">
+    <Title size="8xl" />
+  </div>
+</div>
+
+
   );
 }
